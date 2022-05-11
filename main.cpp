6afa6a578b20356048eb088006c3b135fa446bc5
RyutoOzawa <k021g1163@g.neec.ac.jp>
2022-05-11 15:04:49 +0900

    04_01

--- main.cpp
+++ main.cpp
@@ -266,24 +266,29 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	ConstBufferDataMaterial* constMapMaterial = nullptr;
 	result = constBufferDataMaterial->Map(0, nullptr, (void**)&constMapMaterial);//マッピング
 	assert(SUCCEEDED(result));
-	
+
 	//値を書き込むと自動的に転送される
 	constMapMaterial->color = XMFLOAT4(1, 1, 1, 1.0f);		//RGBAで半透明の赤
 
+	//頂点データ構造体
+	struct Vertex {
+		XMFLOAT3 pos;//xyz座標
+		XMFLOAT2 uv;//uv座標
+	};
+
 	// 頂点データ
-	XMFLOAT3 vertices[] = {
-	{ -0.5f, -0.5f, 0.0f }, // 左下
-	{+0.5f,-0.5f,0.0f},//右下
-	//{ -0.5f, 0.0f, 0.0f }, // 左中
-	//{ +0.5f, 0.0f, 0.0f }, // 右中
-	{-0.5f,+0.5f,0.0f},//左上
-	{+0.5f,+0.5f,0.0f},//右上
+	Vertex vertices[] = {
+		//     x     y    z       u    v
+			{{-0.4f,-0.7f,0.0f} ,{0.0f,1.0f}}, // 左下
+			{{-0.4f,+0.7f,0.0f} ,{0.0f,0.0f}}, // 左上
+			{{+0.4f,-0.7f,0.0f} ,{1.0f,1.0f}}, // 右下
+			{{+0.4f,+0.7f,0.0f} ,{1.0f,0.0f}}, // 右上
 
 	};
 
 
 	// 頂点データ全体のサイズ = 頂点データ一つ分のサイズ * 頂点データの要素数
-	UINT sizeVB = static_cast<UINT>(sizeof(XMFLOAT3) * _countof(vertices));
+	UINT sizeVB = static_cast<UINT>(sizeof(vertices[0]) * _countof(vertices));
 
 	// 頂点バッファの設定
 	D3D12_HEAP_PROPERTIES heapProp{}; // ヒープ設定
@@ -310,7 +315,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	assert(SUCCEEDED(result));
 
 	//インデックスデータ
-	uint16_t indices[] = {
+	unsigned short indices[] = {
 		0,1,2,//三角形1つ目
 		1,2,3,//三角形2つ目
 	};
@@ -354,7 +359,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	ibView.SizeInBytes = sizeIB;
 
 	// GPU上のバッファに対応した仮想メモリ(メインメモリ上)を取得
-	XMFLOAT3* vertMap = nullptr;
+	Vertex* vertMap = nullptr;
 	result = vertBuff->Map(0, nullptr, (void**)&vertMap);
 	assert(SUCCEEDED(result));
 	// 全頂点に対して
@@ -371,7 +376,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// 頂点バッファのサイズ
 	vbView.SizeInBytes = sizeVB;
 	// 頂点1つ分のデータサイズ
-	vbView.StrideInBytes = sizeof(XMFLOAT3);
+	vbView.StrideInBytes = sizeof(vertices[0]);
 
 	ID3DBlob* vsBlob = nullptr; // 頂点シェーダオブジェクト
 	ID3DBlob* psBlob = nullptr; // ピクセルシェーダオブジェクト
@@ -424,7 +429,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// 頂点レイアウト
 	D3D12_INPUT_ELEMENT_DESC inputLayout[] = {
-	{
+	{//xyz座標
 	"POSITION",										//セマンティック名
 	0,												//同じセマンティック名が複数ある時に使うインデックス（0でよい）
 	DXGI_FORMAT_R32G32B32_FLOAT,					//要素数とビット数を表す（XYZの3つでfloat型なのでR32G32B32_FLOAT）
@@ -432,6 +437,11 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	D3D12_APPEND_ALIGNED_ELEMENT,					//データのオフセット値(D3D12_APPEND_ALIGNED_ELEMENTだと自動設定)
 	D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA,		//入力データ種別(標準はD3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA)
 	0												//一度に描画するインスタンス数(0でよい)
+	},
+	{//uv座標
+		"TEXCOORD",0,DXGI_FORMAT_R32G32_FLOAT,0,
+		D3D12_APPEND_ALIGNED_ELEMENT,
+		D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA,0
 	},
 		//座標以外に、色、テクスチャUVなどを渡す場合はさらに続ける
 	};
