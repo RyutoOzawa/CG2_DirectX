5bbb33e682df6681bf2d3d7532459022900e294f
ozawaryuto <k021g1163@g.neec.ac.jp>
2022-06-01 14:35:48 +0900

    06_02完了

--- BasicPS.hlsl
+++ BasicPS.hlsl
@@ -5,7 +5,9 @@ SamplerState smp : register(s0);		//0番スロットに設定されたサンプラー
 
 float4 main(VSOutput input) : SV_TARGET
 {
-	return float4(tex.Sample(smp,input.uv));
+	return float4(input.normal,1);	//RGBをそれぞれの法線のXYZ、Aを1で出力
+
+	//return float4(tex.Sample(smp,input.uv));
 
 	//ポリゴンを白で塗りつぶし
 	//return float4(1,1,1,1);
