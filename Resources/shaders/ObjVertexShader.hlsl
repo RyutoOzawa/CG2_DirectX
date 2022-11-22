deb3b56541b6273f6259c34ff936c35fcfee665a
YuyaHiguchi <k021g1263@g.neec.ac.jp>
2022-11-22 22:48:40 +0900

    うんち

--- Resources/shaders/ObjVertexShader.hlsl
+++ Resources/shaders/ObjVertexShader.hlsl
@@ -2,9 +2,14 @@
 
 VSOutput main(float4 pos : POSITION, float3 normal : NORMAL, float2 uv : TEXCOORD)
 {
+	float4 worldNormal = normalize(mul(mat, float4(normal, 0)));
+	//float4 worldPos = mul(mat, pos);
+
 	VSOutput output; // ピクセルシェーダーに渡す値
-	output.svpos = mul(mat, pos);
-	output.normal = normal;
+	output.svpos = mul(mul(mul(projection,view),mat) , pos);
+
+	//output.worldpos = worldPos;
+	output.normal = worldNormal.xyz;
 	output.uv = uv;
 	return output;
 }
