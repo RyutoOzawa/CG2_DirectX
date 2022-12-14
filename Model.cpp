6418d4de8b7761b36afe5e027e4c4c1836251ff2
MSI\xdrag <k021g1163@g.neec.ac.jp>
2022-12-14 16:33:19 +0900

    モデルクラス分け始めた

---
+++ Model.cpp
@@ -0,0 +1,18 @@
+#include"Model.h"
+
+Model * Model::CreateModel(const std::string & filename)
+{
+return nullptr;
+}
+
+void Model::Create()
+{
+}
+
+void Model::LoadTexture(const std::string& directoryPath, const std::string& filename)
+{
+}
+
+void Model::LoadMaterial(const std::string& directoryPath, const std::string& filename)
+{
+}
