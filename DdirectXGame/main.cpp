c46b913a39bd33cea5e2ed710d2e0dd49e389d0e
MSI\xdrag <k021g1163@g.neec.ac.jp>
2023-04-20 10:35:43 +0900

    01_05 FBX_SDK組み込み完了

--- DdirectXGame/main.cpp
+++ DdirectXGame/main.cpp
@@ -1,13 +1,15 @@
 ﻿
 #include"MyGame.h"
 #include"KEngineFramework.h"
-
+#include"fbxsdk.h"
 
 
 //Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	//フレームワークを基底クラスとしたゲームクラスを生成
 	std::unique_ptr<KEngineFramework> game = std::make_unique< MyGame>();
+
+	FbxManager* fbxManager = FbxManager::Create();
 
 	//ゲームクラスの処理実行
 	game->Run();
