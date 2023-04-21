2d99936ef9e16fd1190c95d244198fe0cea61f04
MSI\xdrag <k021g1163@g.neec.ac.jp>
2023-04-21 15:12:03 +0900

    01_08メッシュの読み込み完了

--- DdirectXGame/engine/3d/FbxLoader.h
+++ DdirectXGame/engine/3d/FbxLoader.h
@@ -48,6 +48,16 @@ public:
 	/// <param name="fbxNode"></param>
 	void ParseNodeRecursive(FbxModel* fbxModel, FbxNode* fbxNode,Node* parent = nullptr);
 
+	/// <summary>
+	/// メッシュ読み取り
+	/// </summary>
+	/// <param name="fbxModel">読み込み先モデルオブジェクト</param>
+	/// <param name="fbxNode">解析対象のノード</param>
+	void ParseMesh(FbxModel* fbxModel, FbxNode* fbxNode);
+
+	//ディレクトリを含んだファイルパスからファイル名を抽出する
+	std::string ExtractFileName(const std::string& path);
+
 /// <summary>
 /// メンバ変数
 /// </summary>
@@ -73,6 +83,12 @@ private:
 	//FBXインポータ
 	FbxImporter* fbxImporter = nullptr;
 
-
-
+	//頂点座標読み取り
+	void ParseMeshVertices(FbxModel* fbxModel, FbxMesh* fbxMesh);
+	//面情報読み取り
+	void ParseMeshFaces(FbxModel* fbxModel, FbxMesh* fbxMesh);
+	//マテリアル読み取り
+	void ParseMaterial(FbxModel* fbxModel, FbxNode* fbxNode);
+	//テクスチャ読み込み
+	void LoadTexture(FbxModel* fbxModel, const std::string& fullPath);
 };
