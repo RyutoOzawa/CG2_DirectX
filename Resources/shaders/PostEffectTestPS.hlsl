a4ce690a13071710f256c09fa06a3210a49db4a4
MSI\xdrag <k021g1163@g.neec.ac.jp>
2023-06-06 12:18:32 +0900

    a

--- Resources/shaders/PostEffectTestPS.hlsl
+++ Resources/shaders/PostEffectTestPS.hlsl
@@ -5,8 +5,7 @@ SamplerState smp : register(s0);				//0番スロットに設定されたサンプラー
 
 float4 main(VSOutput input) : SV_TARGET
 {
-	float4 texcolor = tex.Sample(smp,input.uv+ float2(0.5f,0));
-
+	float4 texcolor = tex.Sample(smp,input.uv);
 	return float4(texcolor.rgb,1);
 }
 
