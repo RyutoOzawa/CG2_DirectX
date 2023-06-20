983d681c0a7c872465ae27a6a6ed4b4f39bd44dd
RyutoOzawa <k021g1163@g.neec.ac.jp>
2023-06-20 17:20:06 +0900

    CG3_02_03終わり 頂点データ1点から三角形を出力できるように

--- Resources/shaders/BasicGeometryShader.hlsl
+++ Resources/shaders/BasicGeometryShader.hlsl
@@ -1,16 +1,23 @@
 #include"Obj.hlsli"
 
-[maxvertexcount(1)]
+[maxvertexcount(3)]
 void main(
 	point VSOutput input[1],
-	inout PointStream<GSOutput> output
+	inout TriangleStream<GSOutput> output
 )
 {
 	
     GSOutput element;
-	//頂点情報
+	//共通
     element.normal = input[0].normal;
     element.uv = input[0].uv;
+    //1点目
     element.svpos = input[0].svpos;
+    output.Append(element);
+    //2点目
+    element.svpos = input[0].svpos + float4(10.0f,10.0f,0,0);
+    output.Append(element);
+    //3点目
+    element.svpos = input[0].svpos + float4(10.0f,0,0,0);
     output.Append(element);
 }
