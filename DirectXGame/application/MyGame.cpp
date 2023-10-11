5b37e33910aa6d4ffed5b7fabad833cecea1a4e2
MSI\xdrag <k021g1163@g.neec.ac.jp>
2023-10-11 14:56:22 +0900

    小見戸 漏

--- DirectXGame/application/MyGame.cpp
+++ DirectXGame/application/MyGame.cpp
@@ -15,7 +15,7 @@ void MyGame::Initialize()
 	sceneFactory = std::make_unique< SceneFactory>();
 	GameSceneManager::GetInstance()->SetSceneFactory(sceneFactory.get());
 	//シーンマネージャに最初のシーンをセット
-	GameSceneManager::GetInstance()->ChangeScene("TITLE",falsege);
+	GameSceneManager::GetInstance()->ChangeScene("TITLE",false);
 }
 
 void MyGame::Finalize()
